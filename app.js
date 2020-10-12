87fba0a4da70206d9f27fa4bc1ac2b53fc67dd8a
Sebastian Zawołański <sebastol90@wp.pl>
2020-10-12 19:55:06 +0200

    Create database connection

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const User = require('./models/user');
+
+const userRouter = require('./routes/auth');
+
+require('dotenv').config();
 
 const app = express();
 
@@ -8,4 +15,20 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 8080);
+app.use(userRouter);
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USER_PASS}@cluster0.hmz92.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) console.error(err);
+    else {
+      //   const user = new User({
+      //     email: 'sebastol90@wp.pl',
+      //     password: '1234',
+      //   });
+      //   user.save();
+      app.listen(process.env.PORT || 8080);
+    }
+  },
+);
